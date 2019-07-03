5d160a9c7e9259b93aab427cb15b67bcc5ac66a9
Anna <anna.gromczak@gmail.com>
2019-07-03 20:23:00 +0200

    add menu_glowne and dodaj_wydatek website

---
+++ main.js
@@ -0,0 +1,24 @@
+function pobierzDate()
+{	
+	var saldo = 1250;
+	var dzisiaj = new Date();
+		
+	var dzien = dzisiaj.getDate();
+	var miesiac = dzisiaj.getMonth() + 1;
+	var rok = dzisiaj.getFullYear();
+	
+	if (saldo > 0)
+	{
+		document.getElementById("podsumowanie").innerHTML = "Saldo na dzień: " + dzien + "-" + miesiac + "-" + rok + " wynosi: " + saldo + " PLN. Gratulacje! Świetnie zarządzasz finansami.";
+	}
+	else if (saldo == 0)
+	{
+		document.getElementById("podsumowanie").innerHTML = "Saldo na dzień: " + dzien + "-" + miesiac + "-" + rok + " wynosi: " + saldo + " PLN.";
+	}
+	else if (saldo < 0)
+	{
+		document.getElementById("podsumowanie").innerHTML = "Saldo na dzień: " + dzien + "-" + miesiac + "-" + rok + " wynosi: " + saldo + " PLN. Uważaj, wpadasz w długi!";
+	}
+	
+	
+}
